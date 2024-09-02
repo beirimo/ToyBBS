00969edc1abf418d4e39e462ee1220c953422f8b
coding-faster <berichmore@naver.com>
2024-09-03 01:24:38 +0900

    구조의 이해

--- backend/src/main/java/domain/BBS.java
+++ backend/src/main/java/domain/BBS.java
@@ -8,29 +8,30 @@ import lombok.Setter;
 
 
 
-@Getter
-@Setter
+@Data
 //@Getter, @Setter, @ToString, @EqualsAndHashCode, @RequireArggs Constructor 모두 포함
-//@NoArgsConstructor  //파라미터 없는 기본 생성자 생성
-//@AllArgsConstructor //모든 필드 파라미터로 받는 생성자 생성
+@NoArgsConstructor  //파라미터 없는 기본 생성자 생성
+@AllArgsConstructor //모든 필드 파라미터로 받는 생성자 생성
 public class BBS {
 
 	//필드 정의 
 	private int seq;
 	private String title;
+	private String createdAt;
 	private String contents;
 	
-	//기본 생성자
-	public BBS() {
-		
-	}
-	
-	public BBS(int seq, String title, String contents) {
-		this.seq = seq;
-		this.title = title;
-		this.contents = contents;
-	}
-	
-	
+//	
+//	//기본 생성자
+//	public BBS() {
+//		
+//	}
+//	
+//	public BBS(int seq, String title, String contents) {
+//		this.seq = seq;
+//		this.title = title;
+//		this.contents = contents;
+//	}
+//	lombok이 @Data 어노테이션으로 자동생성해주기에 주석처리.
+//	
 	
 }
